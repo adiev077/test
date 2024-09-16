4d2ada52239605cf33c4c5250058bfcefe4fdc90
adiev077 <bogdan.45kg@gmail.com>
2024-09-16 12:03:31 +0600

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [intervalId, setIntervalId] = useState(null);
+
+  const startInterval = (increment) => {
+    if (intervalId) {
+      clearInterval(intervalId);
+    }
+
+    const id = setInterval(() => {
+      setCount(prevCount => {
+        if (increment && prevCount < 10) {
+          return prevCount + 1;
+        }
+        if (!increment && prevCount > 0) {
+          return prevCount - 1;
+        }
+        clearInterval(id);
+        return prevCount;
+      });
+    }, 1000);
+
+    setIntervalId(id);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="App" style={{
+      height: 700,
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center'
+    }}>
+      <div style={{
+        display: 'flex',
+        gap: 20,
+      }}>
+        <button
+          onClick={() => startInterval(false)}
+          style={{
+            padding: '5px 50px',
+            border: 'none',
+            borderRadius: 5,
+            background: 'red'
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          -
+        </button>
+        <div>{count}</div>
+        <button
+          onClick={() => startInterval(true)}
+          style={{
+            padding: '5px 50px',
+            border: 'none',
+            borderRadius: 5,
+            background: 'blue'
+          }}
+        >
+          +
+        </button>
+      </div>
     </div>
   );
 }
